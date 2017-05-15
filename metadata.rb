60aa907e933a138b1f1b3c7c625c252ef67d613b
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-15 13:47:36 -0700

    added license information

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,11 @@
 name 'beagle-cluster'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures beagle-cluster'
-long_description 'Installs/Configures beagle-cluster'
+maintainer 'Scientific Computing, FredHutch'
+maintainer_email 'scicomp@fhcrc.org'
+license 'Creative Commons Attribution-ShareAlike 4.0'
+description 'Installs/Configures beagle-cluster nodes'
+long_description 'Installs/Configures beagle-cluster nodes'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/beagle-cluster/issues'
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/beagle-cluster'
+issues_url 'https://github.com/FredHutch/beagle-cluster/issues'
+source_url 'https://github.com/FredHutch/beagle-cluster'
