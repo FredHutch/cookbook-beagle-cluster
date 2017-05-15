5c62538e31407b0d3685eaf9ad600e597f35cc4f
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-15 13:23:02 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# beagle-cluster
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: beagle-cluster
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
