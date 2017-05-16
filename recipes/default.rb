7b0ae12dd99a4d0ac7c9e16a0e5c396e64b99556
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-16 09:53:06 -0700

    Load configuration from vault
    load all the configuration items into the node's attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'chef-vault'
+node.override['beagle']['configs'] = chef_vault_item(
+  node['beagle']['vault_name'], 'configs'
+)
