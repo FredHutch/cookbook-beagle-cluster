03b3e39224340810086493f9a5037cc3db17ef63
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-16 09:50:22 -0700

    Updated metadata
     - add "supports" per foodcritic
     - add issues and source urls
     - reorganized

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,12 @@ maintainer_email 'scicomp@fhcrc.org'
 license 'Creative Commons Attribution-ShareAlike 4.0'
 description 'Installs/Configures beagle-cluster nodes'
 long_description 'Installs/Configures beagle-cluster nodes'
-version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/FredHutch/beagle-cluster/issues'
 source_url 'https://github.com/FredHutch/beagle-cluster'
+
+version '0.1.0'
+supports 'ubuntu', '>= 14.04'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'chef-vault', '~> 0.2.0'
