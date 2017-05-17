1a107a18e2086ddd5d06a5f03c10954da240b5a6
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-17 10:41:31 -0700

    Add template for beagle
     - add template and parameter overrides to have slurm-wlm cookbook use
       the template from this cookbook.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 
 node.default['beagle']['vault_name'] = 'beagle_vault'
+node.default['beagle']['cluster_name'] = 'beagle'
+node.default['beagle']['controller'] = 'beagle-ctld'
+node.override['slurm-wlm']['templates']['cookbook'] = 'beagle-cluster'
+node.override['slurm-wlm']['templates']['slurm_conf'] = 'slurm-core.conf.erb'
