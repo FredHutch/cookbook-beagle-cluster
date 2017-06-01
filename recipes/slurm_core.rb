7f30c8678728ae2820e21ec3426b0caa280788f3
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-06-01 15:50:33 -0700

    Add paths and parameters to slurm configuration
     - configure paths via parameters
     - update template to use these paths

--- recipes/slurm_core.rb
+++ recipes/slurm_core.rb
@@ -12,17 +12,33 @@ node.default['slurm-wlm']['config']['slurm']['ClusterName'] = \
 node.default['slurm-wlm']['config']['slurm']['ControlMachine'] = \
   node['beagle']['controller']
 
-directory '/etc/slurm-llnl' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
+# Set paths &c
+node.override['slurm-wlm']['files'] = {
+  'configdir' => node['beagle']['configs']['etcdir'],
+  'rundir' => node['beagle']['configs']['rundir'],
+  'logdir' => node['beagle']['configs']['logdir'],
+  'spooldir' => node['beagle']['configs']['spooldir']
+}
 
-remote_file '/etc/slurm-llnl/slurm-nodes.conf' do
+slurm_paths = {
+  'JobCheckpointDir' => "#{node['beagle']['configs']['spooldir']}/checkpoint",
+  'SlurmdSpoolDir' => "#{node['beagle']['configs']['spooldir']}/slurmd",
+  'StateSaveLocation' => "#{node['beagle']['configs']['spooldir']}/state",
+  'SlurmctldPidFile' => "#{node['beagle']['configs']['rundir']}/slurmctld.pid",
+  'SlurmctldLogFile' => "#{node['beagle']['configs']['logdir']}/slurmctld.log",
+  'SlurmdPidFile' => "#{node['beagle']['configs']['rundir']}/slurmd.%n.pid",
+  'SlurmdLogFile' => "#{node['beagle']['configs']['logdir']}/slurmd.%n.log",
+  'SlurmSchedLogFile' => "#{node['beagle']['configs']['logdir']}/sched.log"
+}
+
+node.override['slurm-wlm']['config']['slurm'] = node['beagle']['configs'].merge(slurm_paths)
+
+include_recipe 'slurm-wlm'
+
+remote_file "#{node['beagle']['configs']['etcdir']}/slurm-nodes.conf" do
   source node['beagle']['configs']['slurm_node_uri']
   mode '0644'
   owner 'root'
   group 'root'
 end
 
-include_recipe 'slurm-wlm'
