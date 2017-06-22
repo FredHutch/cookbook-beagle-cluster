5cd80846d8bd54d6f75c49a5cdb83b8f7bbbb523
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-06-22 16:26:00 -0700

    Add power control
     - add script in $libdir, link "suspend" and "resume" to this

--- recipes/slurm_controller.rb
+++ recipes/slurm_controller.rb
@@ -6,6 +6,20 @@
 
 include_recipe 'slurm-wlm::slurmctld'
 
+template '/var/lib/slurm-llnl/slurm-power-control.py' do
+  source 'slurm-power-control.py.erb'
+  owner 'root'
+  mode '0755'
+end
+
+link '/var/lib/slurm-llnl/resume' do
+  to '/var/lib/slurm-llnl/slurm-power-control.py'
+end
+
+link '/var/lib/slurm-llnl/suspend' do
+  to '/var/lib/slurm-llnl/slurm-power-control.py'
+end
+
 template '/etc/default/slurmctld' do
   source 'slurmctld.default.erb'
   owner 'root'
