9e25b1b15bc555371428b4d829c54796706bb227
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-08-01 16:28:37 -0700

    Add attributes for /app location

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,6 @@ node.default['beagle']['cluster_name'] = 'beagle'
 node.default['beagle']['controller'] = 'beagle-ctld'
 node.override['slurm-wlm']['templates']['cookbook'] = 'beagle-cluster'
 node.override['slurm-wlm']['templates']['slurm_conf'] = 'slurm-core.conf.erb'
+
+node.override['app']['server'] = 'scdata'
+node.override['app']['path'] = '/scdata_01_S20/app'
