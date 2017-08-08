6581b10b3bc123965e0988aa8bfd07e9e338a3fd
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-08-07 18:32:51 -0700

    update service and package information

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,5 +5,12 @@ node.default['beagle']['controller'] = 'beagle-ctld'
 node.override['slurm-wlm']['templates']['cookbook'] = 'beagle-cluster'
 node.override['slurm-wlm']['templates']['slurm_conf'] = 'slurm-core.conf.erb'
 
+node.override['slurm-wlm']['services']['slurmd'] = 'slurmd'
+node.override['slurm-wlm']['services']['slurmctld'] = 'slurmctld'
+
+node.override['slurm-wlm']['packages']['slurmctld'] = 'slurmctld'
+node.override['slurm-wlm']['packages']['slurmd'] = 'slurmd'
+node.override['slurm-wlm']['packages']['slurmdbd'] = 'slurmdbd'
+
 node.override['app']['server'] = 'scdata'
 node.override['app']['path'] = '/scdata_01_S20/app'
