94c7fa516a927f4da73634c84e878f945573fb0e
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-12-05 14:32:15 -0800

    update hack to remove autofs stuff

--- recipes/static_mount_app.rb
+++ recipes/static_mount_app.rb
@@ -5,12 +5,9 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 
-delete_resource(:map_entry, '/app')
-
 delete_lines 'remove auto /app' do
   path node['autofs']['/-']['map']
   pattern '^/app*'
-  notifies :run, 'execute[mount_app]', :immediately
 end
 
 service 'autofs' do
