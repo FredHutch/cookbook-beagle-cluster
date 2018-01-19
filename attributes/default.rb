0c5314a4d0a652de91387956c23bc3fb4b3661c2
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-01-19 12:54:21 -0800

    remove vault capabilities

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,10 +17,24 @@ node.override['slurm-wlm']['packages']['slurmctld'] = 'slurmctld'
 node.override['slurm-wlm']['packages']['slurmd'] = 'slurmd'
 node.override['slurm-wlm']['packages']['slurmdbd'] = 'slurmdbd'
 
-# node.default['beagle']['mounts'] = {
-#   '/app' =>  {
-#     'device' => 'fserv:/path',
-#     'fstype' => 'nfs',
-#     'options' => 'ro'
-#   }
-# }
+node.default['beagle']['configs'] = {
+  'slurm_node_uri' => 'https://s3-us-west-2/bucket/slurm-nodes.conf',
+  'slurm_partitions_uri' => 'https://s3-us-west-2/bucket/slurm-partitions.conf',
+  'spooldir' => '/var/spool/slurm-llnl',
+  'etcdir' => '/etc/slurm-llnl',
+  'rundir' => '/run/slurm-llnl',
+  'logdir' => '/var/log/slurm-llnl',
+  'state_save' => {
+    'device' => '/var/tmp',
+    'fstype' => 'auto',
+    'options' => 'bind'
+  },
+  'slurm_conf' => {
+    'ClusterName' => 'beagle-tk',
+    'ControlMachine' => 'beagle-tk',
+    'AuthType' => 'auth/munge',
+    'CacheGroups' => '0',
+    'SlurmSchedLogLevel' => '0',
+    'StateSaveLocation' => '/var/spool/slurm-llnl/state'
+  }
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,4 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 include_recipe 'chef-vault'
-node.override['beagle']['configs'] = chef_vault_item(
-  node['beagle']['vault_name'], 'configs'
-)
 include_recipe 'beagle-cluster::chef_atboot'
